c391bbbaea6db0c3ebddc368cba9d7faf6126ad2
王敏 <569049370@qq.com>
2020-04-03 14:50:38 +0800

    add container and test package test3

--- packages/test3includetest2/__tests__/test3includetest2.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const test3Includetest2 = require('..');
-
-describe('test3includetest2', () => {
-    it('needs tests');
-});
--- packages/test3includetest2/lib/test3includetest2.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = test3Includetest2;
-
-function test3Includetest2() {
-    // TODO
-}
